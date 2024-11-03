1b26fec151af914f9962187ecd6caa58e8dc0d8f
tait3r <taite.hartman@colorado.edu>
2024-11-02 18:03:20 -0600

    11/2/2024

--- JoystickIR_Reciever/main.py
+++ JoystickIR_Reciever/main.py
@@ -4,40 +4,55 @@ from machine import Pin
 from machine import PWM
 from ir_rx.nec import NEC_8 # Use the NEC 8-bit class
 from ir_rx.print_error import print_error # for debugging
-Led1 = Pin(15, Pin.OUT)
-Led2 = Pin(14, Pin.OUT)
-Led3 = Pin(11, Pin.OUT)
 pwm_rate = 2000
 ain1_ph = Pin(12, Pin.OUT) # Initialize GP14 as an OUTPUT
 ain2_en = PWM(13, freq = pwm_rate, duty_u16 = 0)
+bin1_ph = Pin(14, Pin.OUT) # Initialize GP14 as an OUTPUT
+bin2_en = PWM(15, freq = pwm_rate, duty_u16 = 0)
 pwm = min(max(int(2**16 * abs(1)), 0), 65535)
 # Callback function to execute when an IR code is received
 def ir_callback(data, addr, _):
     print(f"Received NEC command! Data: 0x{data:02X}, Addr: 0x{addr:02X}")
     if data == 1:
         #Led1.toggle()
-        motor_on()
+        motor_Forward()
     elif data == 2:
         #Led2.toggle()
-        motor_toggle()
+        motor_Reverse()
     elif data == 3:
         #Led3.toggle()
-        motor_off()
-# Motor control (0x1, 0x2, 0x3)
-def motor_on():
-    print("Motor ON") # Print to REPL
+        motor_Left()
+    elif data == 4:
+        motor_right()
+# Motor control (0x1, 0x2, 0x3, 0x4)
+def motor_Forward():
+    print("Motor Forward") # Print to REPL
+    ain1_ph.high()
+    ain2_en.duty_u16(65535-pwm)
+    bin1_ph.high()
+    bin2_en.duty_u16(65535-pwm)
+
+def motor_Reverse():
+    print("Motor Reverse") # Print to REPL
     ain1_ph.low()
     ain2_en.duty_u16(pwm)
+    bin1_ph.low()
+    bin2_en.duty_u16(pwm)
 
-def motor_off():
-    print("Motor OFF") # Print to REPL
+
+def motor_Left():
+    print("Motor Left") # Print to REPL
+    ain1_ph.high()
+    ain2_en.duty_u16(65535-pwm)
+    bin1_ph.low()
+    bin2_en.duty_u16(pwm)
+
+def motor_right():
+    print("motor_right")
     ain1_ph.low()
-    ain2_en.duty_u16(0)
-
-def motor_toggle():
-    print("Motor toggle") # Print to REPL
-    ain1_ph.toggle()
-    ain2_en.duty_u16(0)
+    ain2_en.duty_u16(pwm)
+    bin1_ph.high()
+    bin2_en.duty_u16(65535-pwm)
 # Setup the IR receiver
 ir_pin = Pin(17, Pin.IN, Pin.PULL_UP) # Adjust the pin number based on your wiring
 ir_receiver = NEC_8(ir_pin, callback=ir_callback)
